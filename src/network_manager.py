5edb4d08d00a27e856c8dd1f57fe2ce1c1bde7b8
Incred1342003 <bounish2242003@gmail.com>
2025-11-11 16:11:41 +0530

    FIX #1: Reusable feature added for Run_cmd

--- src/network_manager.py
+++ src/network_manager.py
@@ -3,6 +3,7 @@ import time
 from utils.logger import logger
 import subprocess
 from unittest import SkipTest
+from utils.command_runner import run_cmd
 
 class NetworkManager:
     def __init__(self, parent_if="eth0"):
@@ -12,22 +13,11 @@ class NetworkManager:
         self.isFailed = False
         self.count = 0
 
-    async def run_cmd(self, cmd):
-        proc = await asyncio.create_subprocess_shell(
-            cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await proc.communicate()
-        if proc.returncode != 0:
-            raise subprocess.CalledProcessError(proc.returncode, cmd, output=stdout, stderr=stderr)
-        return stdout.decode()
-
     async def wait_for_ip(self, namespace, interface, timeout=2):
         start = time.time()
         while time.time() - start < timeout:
             try:
-                output = await self.run_cmd(f"sudo ip netns exec {namespace} ip -4 addr show {interface}")
+                output = await run_cmd(f"sudo ip netns exec {namespace} ip -4 addr show {interface}")
                 if "inet " in output:
                     ip = output.split("inet ")[1].split()[0]
                     logger.info(f"{namespace} got IP: {ip}")
@@ -41,19 +31,19 @@ class NetworkManager:
     async def cleanup(self):
         logger.info("----- IP is Not Alloated to some client, Aborting -----")
         try:
-            output = await self.run_cmd("sudo ip netns list")
+            output = await run_cmd("sudo ip netns list")
             namespaces = [line.split()[0] for line in output.splitlines() if line]
 
             for ns in namespaces:
                 macvlan = f"macvlan{ns[2:]}"
                 try:
-                    output = await self.run_cmd(f"sudo ip netns exec {ns} ip -4 addr show {macvlan}")
+                    output = await run_cmd(f"sudo ip netns exec {ns} ip -4 addr show {macvlan}")
                     if "inet " in output:
-                        await self.run_cmd(
+                        await run_cmd(
                             f"sudo ip netns exec {ns} dhclient -r {macvlan} "
                             f"-pf /run/dhclient-{ns}.pid -lf /var/lib/dhcp/dhclient-{ns}.leases"
                         )
-                    await self.run_cmd(f"sudo ip netns delete {ns}")
+                    await run_cmd(f"sudo ip netns delete {ns}")
                 except subprocess.CalledProcessError as e:
                     logger.warning(f"Failed to delete {ns}: {e}")
         except subprocess.CalledProcessError as e:
@@ -67,12 +57,12 @@ class NetworkManager:
         self.client_namespaces.append(ns)
 
         try:
-            await self.run_cmd(f"sudo ip netns add {ns}")
-            await self.run_cmd(f"sudo ip link add link {self.parent_if} {macvlan} address {mac} type macvlan mode bridge")
-            await self.run_cmd(f"sudo ip link set {macvlan} netns {ns}")
-            await self.run_cmd(f"sudo ip netns exec {ns} ip link set dev {macvlan} up")
-            await self.run_cmd(f"sudo ip netns exec {ns} ip link set lo up")
-            await self.run_cmd(
+            await run_cmd(f"sudo ip netns add {ns}")
+            await run_cmd(f"sudo ip link add link {self.parent_if} {macvlan} address {mac} type macvlan mode bridge")
+            await run_cmd(f"sudo ip link set {macvlan} netns {ns}")
+            await run_cmd(f"sudo ip netns exec {ns} ip link set dev {macvlan} up")
+            await run_cmd(f"sudo ip netns exec {ns} ip link set lo up")
+            await run_cmd(
                 f"sudo ip netns exec {ns} dhclient -v {macvlan} "
                 f"-pf /run/dhclient-{ns}.pid -lf /var/lib/dhcp/dhclient-{ns}.leases &"
             )
--- src/ping_manager.py
+++ src/ping_manager.py
@@ -2,23 +2,17 @@ import asyncio
 import random
 import time
 from utils.logger import logger
+from utils.command_runner import run_cmd
+
 
 class PingManager:
     def __init__(self, router_ip="192.168.1.1", duration=60):
         self.router_ip = router_ip
         self.duration = duration
 
-    async def run_cmd(self, cmd):
-        proc = await asyncio.create_subprocess_shell(
-            cmd,
-            stdout=asyncio.subprocess.DEVNULL,
-            stderr=asyncio.subprocess.DEVNULL
-        )
-        await proc.communicate()
-
     async def worker(self, ns, end_time):
         while time.time() < end_time:
-            await self.run_cmd(
+            await run_cmd(
                 f"sudo ip netns exec {ns} ping -c 1 8.8.8.8"
             )
             await asyncio.sleep(random.random() * 0.05)
